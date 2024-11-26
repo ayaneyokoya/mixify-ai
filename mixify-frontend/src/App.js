49831b7bd88e2282eaf0b061dce6c9714a33c911
zhiwang24 <zhiwang224@gmail.com>
2024-11-26 12:35:34 -0500

    Updated UI for Mixer

--- mixify-frontend/src/App.js
+++ mixify-frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import SongAnalyzer from './components/SongAnalyzer';
 import Background from './components/Background';
+import AudioMixer from './components/AudioMixer';
 
 function App() {
   return (
     <div className="App">
       <Background/>
       <SongAnalyzer/>
+      <AudioMixer/>
     </div>
   );
 }
